89d2e3c6309662f9fcc97836794136067a8f99d9
GaziTaufiqIslam <gzidev@gmail.com>
2020-03-16 06:31:53 +0530

    Navbar built for Mobile and Desktop Link Library included

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -26,3 +26,47 @@ import axios from 'axios';
 });
 
 */
+
+
+
+var toggleButton = document.querySelector(".toggle-button");
+var dropdown = document.querySelector(".user-dropdown");
+function toggleDropdown() {
+  dropdown.classList.toggle('visible');
+}
+toggleButton.addEventListener("click", toggleDropdown);
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.querySelector("nav");
+//var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > 0) {
+    navbar.classList.remove("above-the-fold");
+  } else {
+    //alert("Scrolling");
+    navbar.classList.add("above-the-fold");
+  }
+}
+
+var hamburgerMenu = document.querySelector(".hamburger-menu");
+var closeMenu = document.querySelector(".close-menu");
+var hamburgerContent = document.querySelector(".hamburger-content");
+var overlay = document.querySelector(".overlay");
+function MenuContentShow() {
+  //alert("Hamburger");
+  overlay.style.display = "block";
+  hamburgerContent.classList.add("hamburger-content-show");
+  
+}
+function MenuContentHide() {
+  //alert("Hamburger");
+  overlay.style.display = "none";
+  hamburgerContent.classList.remove("hamburger-content-show");
+}
+
+hamburgerMenu.addEventListener("click", MenuContentShow);
+closeMenu.addEventListener("click", MenuContentHide);
+overlay.addEventListener("click", MenuContentHide);
