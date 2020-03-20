fd1f90ceff6395508c90d8313d3715b9ccb8e07c
GaziTaufiqIslam <gzidev@gmail.com>
2020-03-20 21:10:47 +0530

    Added Search modal and the follwing changes Home page markup and style minor style changes and corrections in nav, typography, cards, subscribe cta and searchbox styles

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -28,7 +28,7 @@ import axios from 'axios';
 */
 
 
-
+// User Dashboard/Log In Dropwon Toggle
 var toggleButton = document.querySelector(".toggle-button");
 var dropdown = document.querySelector(".user-dropdown");
 function toggleDropdown() {
@@ -37,12 +37,13 @@ function toggleDropdown() {
 toggleButton.addEventListener("click", toggleDropdown);
 
 
-window.onscroll = function() {myFunction()};
+// Sticky Navabar Effect
+window.onscroll = function() {scrollFunction()};
 
 var navbar = document.querySelector("nav");
 //var sticky = navbar.offsetTop;
 
-function myFunction() {
+function scrollFunction() {
   if (window.pageYOffset > 0) {
     navbar.classList.remove("above-the-fold");
   } else {
@@ -51,6 +52,8 @@ function myFunction() {
   }
 }
 
+
+// Hamburger Menu Toggling
 var hamburgerMenu = document.querySelector(".hamburger-menu");
 var closeMenu = document.querySelector(".close-menu");
 var hamburgerContent = document.querySelector(".hamburger-content");
@@ -70,3 +73,79 @@ function MenuContentHide() {
 hamburgerMenu.addEventListener("click", MenuContentShow);
 closeMenu.addEventListener("click", MenuContentHide);
 overlay.addEventListener("click", MenuContentHide);
+
+
+// Toggling Search Modal
+var searchBox = document.querySelectorAll(".search-bar_large, .search-bar_small");
+var closeModal = document.querySelector(".close-modal");
+var searchModal = document.querySelector(".search-modal");
+
+var modalInput = document.querySelectorAll(".modal-input")
+
+var popularResultItems = document.querySelectorAll('.search-results-items[data-value="Popular"]');
+
+var searchResultHeader = document.querySelector(".search-results-header");
+
+var li = document.getElementsByClassName('search-results-items');
+
+// Object.entries(searchBox).map((object) => { console.log(object) });
+
+
+Object.entries(searchBox).map((object) => {
+  object[1].onclick = function() {
+    // alert(" Search Box Clicked");
+    searchModal.style.display = "block";
+    Object.entries(modalInput).map((object) => {
+      object[1].focus();
+    });
+    
+  }
+  
+});
+
+closeModal.onclick = function() {
+  searchModal.style.display = "none";
+  
+  Object.entries(li).map((object) => {
+    object[1].style.display = "none";
+  });
+  Object.entries(popularResultItems).map((object) => {
+    object[1].style.display = "flex";
+  });
+  Object.entries(modalInput).map((object) => {
+    object[1].value = '';
+  });
+
+  searchResultHeader.innerHTML = "Popular Searches";
+}
+
+
+
+// Search Prototype
+
+Object.entries(modalInput).map((object) => {
+  object[1].onkeyup = function() {
+    //  alert(" Search Box Key Pressed");
+    // Declare variables
+    var filter, a, i, txtValue;
+    
+    var input = object[1].value;
+    filter = input.toLowerCase();
+    
+    searchResultHeader.innerHTML = "Search Results";
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      
+      if (!li[i].innerHTML.toLowerCase().includes(filter)) {
+        li[i].style.display = "none";
+      } else {
+        li[i].style.display = "flex";
+      }
+    }
+    
+  }
+
+});
+
+
+
